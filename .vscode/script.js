1aea4eb9b2aa1c3428b1ef1e51be874ce6790cdb
VictoriaAlvesDEV <victoriaalves1999@gmail.com>
2025-10-29 21:05:04 -0300

    arrasei initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,12 @@
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle("light");
+}
+
+// function = palavra reservada para declarar uma função
+// toggleMode = nome da função
+// () = parênteses que indicam que é uma função
+// { } = chaves que delimitam o corpo da função
+// const = palavra reservada para declarar uma constante
+// document.documentElement = seleciona o elemento raiz do documento (geralmente a tag <html>)
+// .classList.toggle("light") = alterna a classe "light" no elemento selecionado
